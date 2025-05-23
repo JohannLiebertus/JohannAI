e0c6da4062236c082731309ed27d154b76650c3b
JohannLiebertus <plavicigor@gmail.com>
2025-05-23 17:35:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ sendBtn.addEventListener("click", () => {
 
   userInput.value = "";
 
-  fetch("http://127.0.0.1:5000/chat", {
+  fetch("https://johannai-2.onrender.com/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
